5ad14246253df8e6ef4b11b61d1d714ad1582c18
Angeline0210 <angeline0210@gmail.com>
2024-02-17 13:58:31 +0800

    create navbar title

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+import Container from "../Container";
+
 const NavBar = () => {
-    return <div>Navbar</div>;
+    return <div className="
+    sticky
+    top-0
+    w-full
+    bg-slate-200
+    z-30
+    shadow-sm
+    
+    "><div className="py-4 border-b-[1px]">
+        <Container>
+            <div
+                className="
+            flex
+            items-center
+            justify-between
+            gap-3
+            md:gap-0
+            ">
+            
+                <Link href="/">AgroTrade</Link>
+                <div className="hidden md:block">Search</div>
+                <div className="flex item-center gap-8 md:gap-12">
+                    <div>CartCount</div>
+                    <div>UserMenu</div>
+                </div>
+            </div>
+        </Container>
+        </div>
+    </div>;
 }
  
 export default NavBar;
